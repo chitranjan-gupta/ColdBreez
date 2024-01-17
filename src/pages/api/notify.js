a8afadec5ae5a85e8fb17ed0a1837d0869d3d706
Chitranjan Kumar Gupta <chitranjankumargupta123@gmail.com>
2024-01-17 15:01:47 +0000

    Still Fixing Push notification on vercel

--- src/pages/api/notify.js
+++ src/pages/api/notify.js
@@ -1,8 +1,10 @@
 import { sql } from "@vercel/postgres";
 import { push } from "@/lib/push-notification";
+import { log } from "next-axiom";
 
 export default async function handler(req, res) {
   if (req.method == "GET") {
+    log.debug("web-notify", { test: "Testing web-notify"});
     res.status(200).json({});
     const subscriptions = await sql`SELECT * FROM Subscriptions;`;
     subscriptions.rows.forEach((subscription) => {
