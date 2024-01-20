4d4200486f10000f10715b7e0860b59d6598fd69
Chitranjan Kumar Gupta <chitranjankumargupta123@gmail.com>
2024-01-20 08:26:21 +0000

    Fixed Push Notification Service on vercel

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -11,10 +11,15 @@ export default async function handler(req, res) {
         subcategory: `${req.body.subcategories[0]}`,
       },
     });
-    res.status(200).json({});
     const subscriptions = await sql`SELECT * FROM Subscriptions;`;
     subscriptions.rows.forEach((subscription) => {
-      push(subscription.json_column, payload);
+      push(subscription.json_column, payload)
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((err) => {
+          res.status(200).json({ err: err });
+        });
     });
   }
 }
